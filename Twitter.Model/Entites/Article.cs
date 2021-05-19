57b777a23487fa07433ae4d8ece48789993fc6f0
gamze <gamseacar@windowslive.com>
2021-05-19 21:33:15 +0300

    Article claası oluşturuldu

---
+++ Twitter.Model/Entites/Article.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Twitter.Model.Entites
+{
+    public class Article
+    {
+        public string ArticleName { get; set; }
+        public string ArticleDescription { get; set; }
+
+    }
+}
