0ede21af77819f4ab48616d74076c5572483843a
gamze <gamseacar@windowslive.com>
2021-05-23 15:23:29 +0300

    Change AccountController

--- Twitter.Project/Controllers/AccountController.cs
+++ Twitter.Project/Controllers/AccountController.cs
@@ -77,47 +77,4 @@ namespace Twitter.Web.Controllers
             return RedirectToAction("Index", "Home");
         }
     }
-
-
-    [HttpPost]
-    public async Task<IActionResult> Login(LoginModel loginModel)
-        {
-            if (LoginUser(loginModel.Username, loginModel.Password))
-            {
-                var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, loginModel.Username)
-        };
-
-                var userIdentity = new ClaimsIdentity(claims, "login");
-
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-
-                //Just redirect to our index after logging in. 
-                return RedirectToAction("Index", "Home");
-            }
-            return View();
-        }
-
-        public async Task<IActionResult> LogOut()
-        {
-            await HttpContext.SignOutAsync();
-
-            return RedirectToAction("Index");
-        }
-
-        private bool LoginUser(string username, string password)
-        {
-            if (username == "cagatay" && password == "123")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
-        }
-    }
 }
